ab5e7db99c01fed9b1ec6b389143cc8441569d5a
AndresBaron21 <AndresBaron2018@outlook.com>
2023-03-08 16:20:30 -0500

    The URL production was update

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,6 +1,6 @@
 const URL = {
     // BASE_URL: 'http://localhost:3001/api', // local
-    BASE_URL: 'https://company-inventories.online/api', // production
+    BASE_URL: 'https://company-inventories.online:3001/api', // production
     BASE_URL_STORAGE: 'http://127.0.0.1:8000/storage/',
 };
 
