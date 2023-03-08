25aac33a3f38282af3ef0e838ff77d5535f6895b
AndresBaron21 <AndresBaron2018@outlook.com>
2023-03-08 01:13:03 -0500

    URL of the server was added

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,5 +1,6 @@
 const URL = {
-    BASE_URL: 'http://localhost:3001/api',
+    // BASE_URL: 'http://localhost:3001/api', // local
+    BASE_URL: 'http://34.200.235.253:3001/api', // production
     BASE_URL_STORAGE: 'http://127.0.0.1:8000/storage/',
 };
 
