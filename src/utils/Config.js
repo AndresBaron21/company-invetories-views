b477511db420dc3e47a0823bdf58e10eaa143f95
AndresBaron21 <AndresBaron2018@outlook.com>
2023-03-08 13:30:56 -0500

    The URL production was update

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,6 +1,6 @@
 const URL = {
     // BASE_URL: 'http://localhost:3001/api', // local
-    BASE_URL: 'http://34.200.235.253:3001/api', // production
+    BASE_URL: 'http://ec2-3-91-53-193.compute-1.amazonaws.com:3001/api', // production
     BASE_URL_STORAGE: 'http://127.0.0.1:8000/storage/',
 };
 
