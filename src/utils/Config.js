5356a3d1195e20798db30c72a4cbc3851fb4922e
AndresBaron21 <AndresBaron2018@outlook.com>
2023-03-08 16:17:22 -0500

    The URL production was update

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,6 +1,6 @@
 const URL = {
     // BASE_URL: 'http://localhost:3001/api', // local
-    BASE_URL: 'http://ec2-3-91-53-193.compute-1.amazonaws.com:3001/api', // production
+    BASE_URL: 'https://company-inventories.online/api', // production
     BASE_URL_STORAGE: 'http://127.0.0.1:8000/storage/',
 };
 
