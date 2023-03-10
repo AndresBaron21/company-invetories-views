5a1bb09b65b2992c61159aceafcd4b5daa4d74a5
AndresBaron21 <AndresBaron2018@outlook.com>
2023-03-09 22:58:54 -0500

    URL of the server was added

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,6 +1,6 @@
 const URL = {
     // BASE_URL: 'http://localhost:3001/api', // local
-    BASE_URL: 'http://company-inventories.nualtec.com:3001/api', // production1
+    BASE_URL: 'http://ec2-3-228-11-156.compute-1.amazonaws.com:3001/api', // production1
     BASE_URL_STORAGE: '',
 };
 
